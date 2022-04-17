50d3018c0df6f14d3294f9f731b7646df91b8519
Akram MECHERI <mecheri.akram@gmail.com>
2022-04-18 01:50:11 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
   import preact from '@astrojs/preact';
+  import sitemap from '@astrojs/sitemap';
+
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
 	// Enable the Preact renderer to support Preact JSX components.
-	ibtegrations: [preact()],
+	site: 'https://frenchtechlead.com',
+	ibtegrations: [sitemap(), preact()],
 });
