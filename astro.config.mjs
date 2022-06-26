faadbbdb9ec995ec533747c9ad8121ff43484080
Akram MECHERI <mecheri.akram@gmail.com>
2022-06-26 21:47:45 +0200

    sdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
-import preact from '@astrojs/preact';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config'
+import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://frenchtechlead.com',
-  integrations: [sitemap(), preact()]
+  site: "https://frenchtechlead.com",
+  integrations: [
+    sitemap(),
+    preact(),
+    partytown({
+      // Example: Disable debug mode.
+      config: { debug: true },
+    }),
+  ],
 });
