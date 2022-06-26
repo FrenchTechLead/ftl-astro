7eb14e7fce72607c016990a195eb84b7a3497d9e
Akram MECHERI <mecheri.akram@gmail.com>
2022-06-26 22:16:02 +0200

    sss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://frenchtechlead.com",
-  integrations: [sitemap(), preact(), partytown()],
+  integrations: [sitemap(), preact()],
 });
