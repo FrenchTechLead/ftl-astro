9437ff482bbe3ccbb0c374fa1d3c7422b5f21289
Akram MECHERI <mecheri.akram@gmail.com>
2023-01-10 14:01:32 +0100

    get cover image dynamically

---
+++ src/utils/types.ts
@@ -0,0 +1,3 @@
+import type { AstroGlobal } from "astro";
+
+export type AstroType = Readonly<AstroGlobal<Record<string, any>>>;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import type { AstroType } from "./types";
+
 export function onlyUniqueArrayFilter(
   value: string,
   index: number,
@@ -6,6 +8,12 @@ export function onlyUniqueArrayFilter(
   return self.indexOf(value) === index;
 }
 
-export async function getTechPosts() {
+export async function getTechPosts(Astro: AstroType) {
   return await Astro.glob("./posts/tech/*.mdx");
+}
+
+export function getFileName(Astro: AstroType) {
+  const url: string = Astro.props.url;
+  const lastIndex = url.lastIndexOf("/");
+  return url.substring(lastIndex + 1);
 }
